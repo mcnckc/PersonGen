6f57dfc18174744efb527692cabad4b1a1b302b2
mcnckc <s20b1_gusev@179.ru>
2025-10-14 22:06:33 +0300

    cast logged image to float16

--- src/trainer/base_trainer.py
+++ src/trainer/base_trainer.py
@@ -496,7 +496,7 @@ class BaseTrainer:
         #print('logging image', batch["image"].shape, batch["image"])
         self.writer.add_image(
             image_name=mode,
-            image=F.to_pil_image(batch["image"].squeeze(dim=0).cpu()),
+            image=F.to_pil_image(batch["image"].squeeze(dim=0).to(torch.float16).cpu()),
         )
         
 
