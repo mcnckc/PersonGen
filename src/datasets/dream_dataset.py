dca61388bf5d703c565142cb3979837f13543b81
mcnckc <s20b1_gusev@179.ru>
2025-10-15 11:33:56 +0300

    move src images to reward

--- src/datasets/dream_dataset.py
+++ src/datasets/dream_dataset.py
@@ -5,45 +5,16 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 class DreamDataset(Dataset):
-    def __init__(self, img_dir, target_prompt, all_models_with_tokenizer, transform=None, target_size=(512, 512)):
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_size = target_size
+    def __init__(self, target_prompt, all_models_with_tokenizer):
         self.target_prompt = target_prompt
         self.all_models_with_tokenizer = all_models_with_tokenizer
-        if self.transform is None:
-            self.transform = transforms.Compose([
-                transforms.Resize(self.target_size),
-                transforms.ToTensor()
-            ])
-        
-        self.image_paths = []
-        valid_extensions = {'.jpg', '.jpeg', '.png'}
-        
-        for filename in os.listdir(img_dir):
-            ext = os.path.splitext(filename)[1].lower()
-            if ext in valid_extensions:
-                self.image_paths.append(os.path.join(img_dir, filename))
-        
-        if not self.image_paths:
-            raise ValueError(f"Directory {img_dir} has no images")
-
 
     def __len__(self):
         return 1
 
     def __getitem__(self, idx):
-        res = {"prompt":self.target_prompt, "images":[]}
+        res = {"prompt":self.target_prompt}
         for model in self.all_models_with_tokenizer:
             res.update(model.tokenize(self.target_prompt))
-        img_path = self.image_paths[idx]
-        
-        for img_path in self.image_paths:
-            image = Image.open(img_path).convert('RGB')
-            image = image.resize(self.target_size, Image.Resampling.LANCZOS)
-            if self.transform:
-                image = self.transform(image)
-            res["images"].append(image)
-        res["src_images"] = torch.stack(res["images"], dim=0)
         return res
     
--- src/reward_models/clip_ti.py
+++ src/reward_models/clip_ti.py
@@ -2,7 +2,8 @@ import typing as tp
 
 import clip
 import torch
-
+import os
+from PIL import Image
 from src.constants.dataset import DatasetColumns
 from src.reward_models.base_model import BaseModel
 from torchvision import transforms
@@ -13,12 +14,13 @@ MODEL_SUFFIX = "ClipTI"
 
 
 class ClipTI(BaseModel):
-    def __init__(self, device: torch.device, clip_prompt: str):
+    def __init__(self, device: torch.device, clip_prompt: str, src_img_dir: str):
         super().__init__(
             model_suffix=MODEL_SUFFIX, reward_scale_factor=1, reward_offset=0
         )
 
         model, transform = clip.load(MODEL_NAME, device=device, jit=False)
+        self.device = device
         self.model = model
         self.clip_resolution = 224
         self.tensor_preproc = transforms.Compose(
@@ -39,11 +41,24 @@ class ClipTI(BaseModel):
                 clip_prompt,
                 truncate=True,
             ).to(device))
-            
-        print("Grad1:", self.tg_prompt.requires_grad)
-        self.tg_prompt = torch.nn.functional.normalize(self.tg_prompt, dim=-1)
-        print("Grad2:", self.tg_prompt.requires_grad)
-        self.device = device
+            self.tg_prompt = torch.nn.functional.normalize(self.tg_prompt, dim=-1)
+
+            src_image_paths = []
+            src_images = []
+            valid_extensions = {'.jpg', '.jpeg', '.png'}
+            for filename in os.listdir(src_img_dir):
+                ext = os.path.splitext(filename)[1].lower()
+                if ext in valid_extensions:
+                    src_image_paths.append(os.path.join(src_img_dir, filename))
+            if not src_image_paths:
+                raise ValueError(f"Directory {src_img_dir} has no images")
+            for img_path in self.src_image_paths:
+                image = Image.open(img_path)
+                src_images.append(transform(image))
+            src_images = torch.stack(src_images, dim=0).to(device)
+            self.src_images = self.model.encode_image(src_images)
+            self.src_images = torch.nn.functional.normalize(src_images, dim=-1)
+        
 
     def tokenize(self, caption: str) -> tp.Dict[str, torch.Tensor]:
         processed_caption = clip.tokenize(
@@ -61,11 +76,9 @@ class ClipTI(BaseModel):
         image: torch.Tensor,
     ) -> torch.Tensor:
         tg_image = self.model.encode_image(self.tensor_preproc(image))
-        src_images = self.model.encode_image(self.tensor_preproc(batch["src_images"]))
         tg_image = torch.nn.functional.normalize(tg_image, dim=-1)
-        src_images = torch.nn.functional.normalize(src_images, dim=-1)
         clipT = self.tg_prompt @ tg_image.T
-        clipI = tg_image @ src_images.T
+        clipI = tg_image @ self.src_images.T
         clipT = clipT.mean()
         clipI = clipI.mean()
         batch["clip_t"] = clipT
