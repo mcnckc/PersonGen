68e8e64012fc57e49068f8d85dbec7587baf7aed
mcnckc <s20b1_gusev@179.ru>
2025-11-27 16:28:02 +0300

    fix val dataset

--- src/datasets/dream_dataset.py
+++ src/datasets/dream_dataset.py
@@ -5,12 +5,13 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 class DreamDataset(Dataset):
-    def __init__(self, target_prompt, all_models_with_tokenizer):
+    def __init__(self, target_prompt, all_models_with_tokenizer, inf_dataset=False):
         self.target_prompt = target_prompt
         self.all_models_with_tokenizer = all_models_with_tokenizer
+        self.len = 1000000000 if inf_dataset else 1
 
     def __len__(self):
-        return 1000000000
+        return self.len
 
     def __getitem__(self, idx):
         res = {"prompt":self.target_prompt}
